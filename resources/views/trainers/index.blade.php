f174dd17c10e1a60145ed647523934522a139835
Bryan <bsteven1993@gmail.com>
2019-04-17 22:58:22 -0500

    ajuste en la imagen de la vista de trainers

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -9,8 +9,8 @@
     </div>
     <div class="row justify-content-center align-items-start">
         @foreach($trainers as $trainer)
-            <div class="card mx-3 my-3" style="width: 18rem;">
-                {{--<img class="card-img-top" src="..." alt="Card image cap">--}}
+            <div class="card mx-3 my-3 text-center" style="width: 18rem;">
+                <img class="card-img-top rounded-circle mx-auto my-3" style="height: 150px; width: 150px;" src="imagenes/{{$trainer->avatar}}" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">{{$trainer->name}}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
